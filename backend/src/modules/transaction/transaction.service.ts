ffebe4a839a52b5b56a0f1804f976587c54214aa
Alain MUCYO <alainmucyo3@gmail.com>
2021-07-14 12:07:01 +0200

    Transactions and account balance

---
+++ backend/src/modules/transaction/transaction.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@nestjs/common";
+import { CreateTransactionDto } from "./dto/create-transaction.dto";
+import { Transaction, TransactionType } from "./entities/transaction.entity";
+import { User } from "../user/user.entity";
+import { UserService } from "../user/user.service";
+
+@Injectable()
+export class TransactionService {
+  constructor(private readonly userService: UserService) {}
+
+  async create(createTransactionDto: CreateTransactionDto, user: User) {
+    //Getting user transaction
+    const transaction = new Transaction();
+    transaction.type = createTransactionDto.type;
+    transaction.amount = createTransactionDto.amount;
+    transaction.user = user;
+
+    // Computing new balance
+    let balance;
+    if (transaction.type == TransactionType.WITHDRAW)
+      balance = user.balance - transaction.amount;
+    else balance = user.balance + transaction.amount;
+
+    //Saving new balance
+    await transaction.save();
+
+    //Updating user balance;
+    user.balance = await this.userService.updateBalance(user, balance);
+    return transaction;
+  }
+
+  async findByUser(userId: number) {
+    return await Transaction.find({ where: { user: userId } });
+  }
+}
